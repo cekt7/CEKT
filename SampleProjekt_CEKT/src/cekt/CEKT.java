5c8f89e38e3deafc749af897fb4f94f1cbb1e4cc
cekt7 <jetzinger.cornelia@gmail.com>
2015-01-07 01:51:13 +0100

    Initial Changes

--- SampleProjekt_CEKT/src/cekt/CEKT.java
+++ SampleProjekt_CEKT/src/cekt/CEKT.java
@@ -3,5 +3,6 @@ package cekt;
 public class CEKT {
 	public void print() {
 		System.out.println("Erste Aufgabe erledigt => CMap");
+		System.out.println("Beginn der zweiten Aufgabe => Gruppenarbeit");
 	}
 }
