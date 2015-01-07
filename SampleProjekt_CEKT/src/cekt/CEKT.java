75401340313eef294b7512271f317c42eb19e584
cekt7 <jetzinger.cornelia@gmail.com>
2015-01-07 13:49:33 +0100

    include Bug

--- SampleProjekt_CEKT/src/cekt/CEKT.java
+++ SampleProjekt_CEKT/src/cekt/CEKT.java
@@ -4,5 +4,10 @@ public class CEKT {
 	public void print() {
 		System.out.println("Erste Aufgabe erledigt => CMap");
 		System.out.println("Beginn der zweiten Aufgabe => Gruppenarbeit");
+	
+		int[]array = new int[2];
+		
+		int k = array[3];
+		
 	}
 }
